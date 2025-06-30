274086f419a8105be73ce2803c5519da54b2f69e
Aungkokolin1997 <lin@quartile.co>
2025-06-30 06:13:17 +0000

    adj

--- attachment_image_resize/models/ir_attachment.py
+++ attachment_image_resize/models/ir_attachment.py
@@ -17,7 +17,7 @@ class IrAttachment(models.Model):
 
     def _postprocess_contents(self, values):
         self = self.with_context(
-            resize_target_model=self.res_model or values.get("res_model")
+            resize_target_model=values.get("res_model") or self.res_model
         )
         self.resize_done = True
         return super()._postprocess_contents(values)
